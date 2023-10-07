5fda4e7fb81e54e09c4b8483c1ea72aa22a9a76c
Aviral Verma <aviral.robin@gmail.com>
2023-10-07 08:42:02 +0530

    view data in localhost

--- backend/BlogApp/views.py
+++ backend/BlogApp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 
+@api_view(['GET'])
 def home(request):
-    return HttpResponse('Hello world')
+    posts = Post.objects.all()
+    serialize = PostSerializer(posts, many = True)
+    return HttpResponse(serialize.data)
