16f9f1bc1d78b8ec300474dad80fcc5e9682377d
Aviral Verma <aviral.robin@gmail.com>
2023-10-09 20:28:21 +0530

    Install & configure Tailwind

--- frontend/blog-app/src/App.tsx
+++ frontend/blog-app/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="text-slate-900 bg-purple-400 text-3xl font-bold underline">Hello world</h1>
     </div>
   )
 }
