b52b197744015db4b2afd8180fd31548a138d200
Aviral Verma <aviral.robin@gmail.com>
2023-10-16 01:15:14 +0530

    Modify apiURL = "http://127.0.0.1:8000/blog";

--- frontend/blog-app/src/features/blogs/BlogExcerpt.tsx
+++ frontend/blog-app/src/features/blogs/BlogExcerpt.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BloggerSliceState, PostType } from './BloggerSlice';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-interface BlogExcerptProps {
-  post: PostType;
+import { Link } from 'react-router-dom';
+
+interface OneBlogExcerptProps {
+  post: PostType,
+  postId: number
 }
 
-const BlogExcerpt: React.FC<BlogExcerptProps> = ({ post }) => {
+const OneBlogExcerpt: React.FC<OneBlogExcerptProps> = ({ post, postId }) => {
 
   // Destructuring post
   const { id, title, description, image_url, date_posted, owner, category  } = post;
@@ -17,7 +20,7 @@ const BlogExcerpt: React.FC<BlogExcerptProps> = ({ post }) => {
     <div className='w-80 shadow-lg' >
       {/* Access post and postId in your component */}
       <img className='object-cover rounded-lg w-full h-72'
-      src={image_url} alt={`${title}`} />
+      src={image_url} alt={`${id}`} />
       <div className='mb-3'>
         <div className='flex justify-between items-center px-3 mt-3 bg-purple-600 text-white font-semibold'> 
           <div>
@@ -30,11 +33,15 @@ const BlogExcerpt: React.FC<BlogExcerptProps> = ({ post }) => {
         </div>
         {/* <p>Post ID: {id}</p> */}
         <h1 className='font-bold' >{title}</h1>
-        <p>{description.substring(0,90)}... <button><span className='font-bold text-blue-500 underline'>Read more</span></button> </p>
+        <p>{description.substring(0,90)}... 
+          <Link to={`blog/${id}`}>
+            <span className='font-bold text-blue-500 underline'>Read more</span>
+          </Link>
+        </p>
         {/* <p>Post = {post}</p> */}
       </div>
     </div>
   );
 };
 
-export default BlogExcerpt;
+export default OneBlogExcerpt;
